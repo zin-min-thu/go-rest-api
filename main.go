8060f0027d3f79f8b4f4d12f69e52b2e336dc40c
KoZin <zinminthu.dev@gmail.com>
2025-05-15 22:33:58 +0700

    setup a first route

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
 
+	server.LoadHTMLGlob("templates/*")
+
+	// server.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+	// 		"title": "Go REST API Project",
+	// 	})
+	// })
+	server.GET("/", getHome)
+
+	server.GET("/events", getEvents)
+
 	server.Run(":8080") // localhost:8080
+}
+
+func getHome(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title": "Go REST API Project",
+	})
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Events"})
 }
