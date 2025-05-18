72b5eaf2a40972cc1506a0da35c9ddcbdc4dacdc
KoZin <zinminthu.dev@gmail.com>
2025-05-18 21:10:39 +0700

    create get and post api request

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/zin-min-thu/go-rest-api/models"
 )
 
 func main() {
@@ -19,6 +20,7 @@ func main() {
 	server.GET("/", getHome)
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") // localhost:8080
 }
@@ -30,5 +32,22 @@ func getHome(c *gin.Context) {
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Events"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": err})
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
