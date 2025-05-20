10ce01d010adc91cee954aec633cb670c31e110a
KoZin <zinminthu.dev@gmail.com>
2025-05-20 22:31:43 +0700

    get single event

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/zin-min-thu/go-rest-api/db"
@@ -24,6 +25,7 @@ func main() {
 	server.GET("/", getHome)
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080") // localhost:8080
@@ -47,6 +49,24 @@ func getEvents(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusOK, gin.H{"events": events})
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse event id"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"event": event})
 }
 
 func createEvent(context *gin.Context) {
