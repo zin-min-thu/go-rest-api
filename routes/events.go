a3389d7c6df85ec658caba4b8f353b68eb068b4e
KoZin <zinminthu.dev@gmail.com>
2025-07-17 22:17:32 +0700

    adding token verification

--- routes/events.go
+++ routes/events.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/zin-min-thu/go-rest-api/models"
+	"github.com/zin-min-thu/go-rest-api/utils"
 )
 
 func getEvents(context *gin.Context) {
@@ -41,9 +42,24 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
+	err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized"})
+		return
+	}
+
 	var event models.Event
 
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": err})
 	}
